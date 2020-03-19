78316d6a1352963be116dc58a6b120c6c96d33c3
Marc Shayne <marcshayne@hotmail.com>
2020-03-19 08:33:27 -0400

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        // tell the datacontext class about our entities
+        // Values will be the  table name
+        public DbSet<Value> Values { get; set; }
+    }
+}
